4ac0c400712633887a33d95959dd103dfac4deca
diplomat-bit <diplomat@citibankdemobusiness.dev>
2026-02-28 19:35:07 -0500

    Set base path for Vite server to '/ai-news/'

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
+        base: '/ai-news/', 
         host: '0.0.0.0',
       },
       plugins: [react()],
